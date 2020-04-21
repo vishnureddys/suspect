6f80b536e044ab5aa3a63ea60c6fc23743b2118d
Vishnu S Reddy <52468309+Vishnu-S-Reddy@users.noreply.github.com>
2020-04-21 17:08:49 +0530

    Have added the login authentication to the posts page.
    Now without logging in, they can't view the /posts page.

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 from django.views.generic import ListView
 from .models import Post
 
 
-class PostsPageView(ListView):
+class PostsPageView(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'posts.html'
